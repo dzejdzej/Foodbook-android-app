dc27a3bc648f3849ab0b906288005082b0124fec
babicmilan <milan.babic@hybrid-it.rs>
2018-06-12 22:29:23 +0200

    finished localization

--- foodbook/src/main/java/com/robpercival/demoapp/activities/InviteFriendsActivity.java
+++ foodbook/src/main/java/com/robpercival/demoapp/activities/InviteFriendsActivity.java
@@ -1,7 +1,10 @@
 package com.robpercival.demoapp.activities;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -33,6 +36,7 @@ import com.robpercival.demoapp.state.ApplicationState;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by User on 6/3/2018.
@@ -53,7 +57,7 @@ public class InviteFriendsActivity extends AppCompatActivity implements InviteFr
         super.onCreate(icicle);
         setContentView(R.layout.activity_invite_friends);
 
-        setTitle("Invite friends");
+        setTitle(R.string.inviteFriends);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -237,6 +241,35 @@ public class InviteFriendsActivity extends AppCompatActivity implements InviteFr
                 intent = new Intent(InviteFriendsActivity.this, MyReservationsActivity.class);
                 InviteFriendsActivity.this.startActivity(intent);
                 //MainActivity.this.finish();
+                break;
+            case R.id.nav_menu3:
+                CharSequence languages[] = new CharSequence[] {"Serbian", "English"};
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle(R.string.selectLanguage);
+                builder.setItems(languages, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(which == 0) {
+                            Locale locale = new Locale("sr");
+                            Configuration config = getBaseContext().getResources().getConfiguration();
+                            config.locale = locale;
+                            getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+                            Intent intent = new Intent(InviteFriendsActivity.this, InviteFriendsActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                        } else {
+                            Locale locale = new Locale("en");
+                            Configuration config = getBaseContext().getResources().getConfiguration();
+                            config.locale = locale;
+                            getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+                            Intent intent = new Intent(InviteFriendsActivity.this, InviteFriendsActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                        }
+                    }
+                });
+                builder.show();
                 break;
             case R.id.nav_menu4:
                 intent = new Intent(InviteFriendsActivity.this, ChangePasswordActivity.class);
